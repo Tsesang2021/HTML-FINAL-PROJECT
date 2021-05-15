3625b833390add7b73935c58dd22eff22765e429
Tsesang Dolkar <tsesang125@gmail.com>
2021-05-15 19:29:33 +0200

    changed photo

---
+++ src/script.js
@@ -0,0 +1,11 @@
+
+            function ContactMe (){
+            let name=prompt("What is your name?");
+            let food=prompt("Which is your favorite food?");
+                alert("😊 Hi" +" "+ name+"! Try my Momo recipe.");
+                } 
+            
+             let ApplyButton=document.querySelector("button");
+             ApplyButton.addEventListener("click",ContactMe );
+
+        
